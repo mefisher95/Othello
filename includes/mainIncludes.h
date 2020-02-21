d590c179225b93899b5f38522d67cda1e67069d6
Michael Fisher <56176130+mefisher95@users.noreply.github.com>
2020-02-21 00:39:28 -0600

    Add files via upload

---
+++ includes/mainIncludes.h
@@ -0,0 +1,21 @@
+/********************************************************************************
+/ Project: Othello
+/--------------------------------------------------------------------------------
+/ By: Michael Fisher, 2019
+=================================================================================
+/ File: main.cpp
+/
+/ Header file that contians the neccesary included header files for using the
+/ SDL Library
+********************************************************************************/
+
+#ifndef MAININCLUDES_H
+#define MAININCLUDES_H
+
+#include "Includes.h"
+#include "Constants.h"
+#include "compgeom.h"
+#include "Surface.h"
+#include "Event.h"
+
+#endif
